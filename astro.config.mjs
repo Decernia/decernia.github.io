1b9037a404486bc0b8e4c5c5acf7a1719a1c5bac
Pieter-Decernia <pieter@decernia.nl>
2026-08-17 14:15:33 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: 'https://screwfast.uk',
+  site: 'https://decernia.github.io',
+  base: '/website',
   image: {
     domains: ['images.unsplash.com'],
   },
