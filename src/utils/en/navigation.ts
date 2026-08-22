f39f62ad552e0e8ad85fdebcb9d7648f56020c03
Pieter-Decernia <pieter@decernia.nl>
2026-08-22 13:54:18 +0200

    Taal en style: code formatting bijgewerkt
    Prettier en format scripts toegepast op alle gewijzigde bestanden.

---
+++ src/utils/en/navigation.ts
@@ -0,0 +1,43 @@
+// src/utils/en/navigation.ts
+
+const navBarLinks = [
+  { name: 'Home', url: '/en' },
+  { name: 'Products', url: '/en/products' },
+  { name: 'Services', url: '/en/services' },
+  { name: 'Blog', url: '/en/blog' },
+  { name: 'Contact', url: '/en/contact' },
+];
+
+const footerLinks = [
+  {
+    section: 'Ecosystem',
+    links: [
+      { name: 'Documentation', url: '/welcome-to-docs/' },
+      { name: 'Tools & Equipment', url: '/en/products' },
+      { name: 'Services', url: '/en/services' },
+    ],
+  },
+  {
+    section: 'Company',
+    links: [
+      { name: 'About us', url: '#' },
+      { name: 'Blog', url: '/en/blog' },
+      { name: 'Careers', url: '#' },
+      { name: 'Customers', url: '#' },
+    ],
+  },
+];
+
+const socialLinks = {
+  facebook: '#',
+  x: '#',
+  github: 'https://github.com/mearashadowfax/ScrewFast',
+  google: '#',
+  slack: '#',
+};
+
+export default {
+  navBarLinks,
+  footerLinks,
+  socialLinks,
+};
